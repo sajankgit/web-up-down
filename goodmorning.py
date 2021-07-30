234b5ab9200e8eefa8e5082c794189734911e0f1
sajankgit <37524392+sajankgit@users.noreply.github.com>
2021-07-30 19:30:01 +0530

    Create goodmorning.py

---
+++ goodmorning.py
@@ -0,0 +1,21 @@
+def lambda_handler(event, context):
+    import boto3
+    from botocore.exceptions import ClientError
+    client = boto3.client('elbv2')
+    response = response = client.modify_listener(
+        ListenerArn='<copy Listener ARN from loadbalancer>',
+        Protocol='HTTPS',
+        DefaultActions=[
+            {
+            'Type': 'forward',
+            'ForwardConfig': {
+                'TargetGroups': [
+                    {
+                    'TargetGroupArn': '<copy Target group ARN from target groups of live webapplication>' 
+                    }
+                ]
+            }
+
+            }
+            ]
+        
