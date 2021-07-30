872f497481b40c8aa5f2563305048c6d4609a2d8
sajankgit <37524392+sajankgit@users.noreply.github.com>
2021-07-30 19:32:08 +0530

    Create goodnight.py

---
+++ goodnight.py
@@ -0,0 +1,22 @@
+def lambda_handler(event, context):
+    import boto3
+    from botocore.exceptions import ClientError
+    client = boto3.client('elbv2')
+    response = response = client.modify_listener(
+        ListenerArn='<<copy Listener ARN from loadbalancer> ',
+        Protocol='HTTPS',
+        DefaultActions=[
+            {
+            'Type': 'forward',
+            'ForwardConfig': {
+                'TargetGroups': [
+                    {
+                    'TargetGroupArn': '<copy Target group ARN from target group of goodnight server>' 
+                    }
+                ]
+            }
+
+            }
+            ]
+        
+    )
